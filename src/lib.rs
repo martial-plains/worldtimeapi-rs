c74cce56687e1037d47eac9571263afa549d0149
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2022-12-14 09:22:31 -0400

    Update version

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! worldtimeapi = "0.2.0"
+//! worldtimeapi = "0.4.1"
 //! ```
 //!
 //! Then create a client for an endpoint (currently they only offer "ip" and "timezone"):
